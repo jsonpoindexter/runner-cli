08e7a5ce76611e7b15776d5122ee02f348e40a27
Jason Poindexter <poindexter.json@gmail.com>
2023-02-13 13:24:18 -0800

    Make into pip package / installable / executable

--- runner_cli/__init__.py
+++ runner_cli/__init__.py
@@ -10,6 +10,8 @@ import json
 
 working_directory = os.getcwd()
 
+print("Working directory: ", working_directory)
+
 
 # Assume we are in root of project. Project dir:
 # root/
@@ -29,6 +31,7 @@ def parse_package_scripts():
     if os.path.exists('./packages'):
         os.chdir(working_directory + '/packages')
         packages = [d for d in os.listdir('.') if os.path.isdir(d)]
+        print("Found packages: ", ", ".join(packages))
         for package in packages:
             package_json_path = f'./{package}/package.json'
             if not os.path.isfile(package_json_path):
@@ -41,14 +44,14 @@ def parse_package_scripts():
         raise Exception("Tool needs to be run in directory where 'packages' as a direct sub-directory")
 
 class RunnerCli(cmd.Cmd):
-
+    prompt = 'runner-cli: '
     def preloop(self):
         self.do_help("")
 
     def do_EOF(self, line):
         return True
 
-if __name__ == '__main__':
+def main():
     scripts_dict = parse_package_scripts()
 
     # print(scripts_dict)
@@ -57,29 +60,33 @@ if __name__ == '__main__':
         main_method_name = f"do_{package_name}"
         script_names = list(package_scripts_dict.keys())
         exec_string = f"def {main_method_name}(self, script_name): \n \
-            \t\"\"\"{package_name} [script]\n \
-            Execute {package_name} script: {script_names}\"\"\"\n \
-            \tif script_name and script_name in {script_names}:\n \
-                \t\tcommand = f'cd ./packages/{package_name} && yarn run {{script_name}}'\n \
-                \t\tprint(command)\n \
-                \t\tresult = subprocess.run(command,stdout=subprocess.PIPE, cwd='{working_directory}',shell=True)\n \
-                \t\tprint(result.stdout.decode('utf-8')) \
-        "
-        exec(exec_string) # Create do_*main_method_name* cmd command
+              \t\"\"\"{package_name} [script]\n \
+              Execute {package_name} script: {script_names}\"\"\"\n \
+              \tif script_name and script_name in {script_names}:\n \
+                  \t\tcommand = f'cd ./packages/{package_name} && yarn run {{script_name}}'\n \
+                  \t\tprint(command)\n \
+                  \t\tresult = subprocess.run(command,stdout=subprocess.PIPE, cwd='{working_directory}',shell=True)\n \
+                  \t\tprint(result.stdout.decode('utf-8')) \
+          "
+        # print(exec_string)
+        exec(exec_string)  # Create do_*main_method_name* cmd command
         main_method = locals()[main_method_name]
         setattr(RunnerCli, main_method_name, main_method)
 
         # Create auto-completion for main method ie: complete_accounts
         complete_method_name = f"complete_{package_name}"
         exec_string = f"def {complete_method_name}(self, text, line, begidx, endidx):\n \
-            \tif not text:\n \
-                \t\tcompletions = {script_names}[:]\n \
-            \telse:\n \
-                \t\tcompletions = [ f \n\
-                                \t\t\tfor f in {script_names}\n \
-                                \t\t\tif f.startswith(text)\n \
-                                \t\t]\n \
-            \treturn completions"
+              \tprint('Complete')\n \
+              \tif not text:\n \
+                  \t\tcompletions = {script_names}[:]\n \
+              \telse:\n \
+                  \t\tcompletions = [ f \n\
+                                  \t\t\tfor f in {script_names}\n \
+                                  \t\t\tif f.startswith(text)\n \
+                                  \t\t]\n \
+              \treturn completions"
+
+        # print(exec_string)
         exec(exec_string)  # Create do_*main_method_name* cmd command
         complete_method = locals()[complete_method_name]
         setattr(RunnerCli, complete_method_name, complete_method)
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+#
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
+    name="runner-cli-pkg-jsonpoindexter",
+    version="0.0.1",
+    author="Jason Poindexter",
+    author_email="jason.poindexter@lifescoremusic.com",
+    description="Run package scripts",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    # url="https://github.com/jsonpoindexter/uSerialSync",
+    # packages=setuptools.find_packages(),
+    packages=['runner_cli'],
+    scripts=['bin/runner_cli'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Operating System :: Microsoft :: Windows :: Windows NT/2000',
+    ],
+    entry_points={'console_scripts': [
+        'runner-cli = runner_cli:main [runner_cli]',
+    ]},
+    python_requires='>=3.6',
+)
